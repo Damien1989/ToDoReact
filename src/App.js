676a24773956460dee27180bb083a24604d158fd
damien <contact.damiendiaz@gmail.com>
2024-07-16 16:11:46 +0200

    form created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,48 @@ function App() {
   return (
    <div>
     <Header />
+
+    <div className='container px-3'>
+
+      <h2 className='title mt-5'>
+        Rentrez vos tâches à faire
+      </h2>
+
+      <form>
+        <div className='field'>
+          <div className='control'>
+            <label htmlFor='tache' className='label'>Tâche</label>
+            <input
+            className='input'
+            type='text'
+             id='tache'
+              placeholder='fais 1 tâche'/>
+          </div>
+        </div>
+
+
+        <div className='field'>
+          <div className='control'>
+            <label htmlFor='txt' className='label'>Contenu de la tâche
+            </label>
+            <textarea
+            className='textarea'
+            type='text'
+             id='txt'
+              placeholder='fais 1 tâche'
+              >
+              </textarea>
+          </div>
+        </div>
+
+        <div className='control'>
+
+          <button type='submit' className='button is-link'>Créer une tâche</button>
+        </div>
+
+
+      </form>
+    </div>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <nav className='navbar has-text-centered has-background-primary' role='navigation' aria-label='main-navigation'>
+    <header className='has-background-primary' role='navigation' aria-label='main-navigation'>
 
-        <h2 className='is-size-4 has-text-white' style={{lineHeight: '52px'}}> Ap TODO List !!</h2>
-    </nav>
+        <h2 className='is-size-4 has-text-centered has-text-white' style={{lineHeight: '52px'}}> App Todo List !!</h2>
+    </header>
   )
 }
