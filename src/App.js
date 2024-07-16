471f2f10d71c5555d2a86f26d1c6f3ebee9599b2
damien <contact.damiendiaz@gmail.com>
2024-07-16 15:42:09 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import '../node_modules/bulma/css/bulma.min.css';
+import Header from './Components/Header/Header.js';
+
+function App() {
+  return (
+   <div>
+    <Header />
+    </div>
+  );
+}
+
+export default App;
